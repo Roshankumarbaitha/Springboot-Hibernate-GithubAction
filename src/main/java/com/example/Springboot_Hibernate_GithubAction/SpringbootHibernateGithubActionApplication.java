c326c3482e4b92438ac21932d956e055ece858a3
Roshan kumar <RoshanKumarbaitha>
2025-01-10 17:25:52 +0530

    springboot-hibernate-githubaction

---
+++ src/main/java/com/example/Springboot_Hibernate_GithubAction/SpringbootHibernateGithubActionApplication.java
@@ -0,0 +1,13 @@
+package com.example.Springboot_Hibernate_GithubAction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHibernateGithubActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHibernateGithubActionApplication.class, args);
+	}
+
+}
